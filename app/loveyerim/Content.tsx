5f6e8a963031b95dd85fbe6cf1a415c4ede6274b
Dujin <dujink2@gmail.com>
2024-08-01 05:22:08 -0700

    gfday update'

--- app/loveyerim/Content.tsx
+++ app/loveyerim/Content.tsx
@@ -1,35 +1,39 @@
 import React from 'react'
 import { Parallax } from 'react-scroll-parallax'
 import Image from 'next/image';
+import LaughMachine from './LaughMachine';
 export default function Content({ setIsConfettiVisible }: { setIsConfettiVisible: any }) {
     const firstDate = new Date('2024-01-18');
     const today = new Date();
     const datingDays = Math.floor((today.getTime() - firstDate.getTime()) / (1000 * 60 * 60 * 24));
     return (
         <>
-            <h1 className='text-3xl mt-[200px]'> Happy Birthday Yerim!</h1>
+            <h1 className='text-3xl mt-[200px]'> Happy Girlfriend day to the best girlfriend!</h1>
             <Parallax speed={10}>
-                <Image src='/svg/cake.svg' alt='cake' width={40} height={40} className='rounded-full mx-auto my-6 w-[160px] h-[160px] object-contain' onClick={() => setIsConfettiVisible(true)} />
+                <Image src='/images/VictoriaCake.png' alt='cake' width={160} height={160} className='rounded-full mx-auto my-6 w-[160px] h-[160px] object-contain' onClick={() => setIsConfettiVisible(true)} />
             </Parallax>
             <Parallax className='my-[100px]' speed={5}>
-                <p>I hope you are having a wonderful birthday :&#41;</p>
-                <p className='mt-2'>It&apos;s been {datingDays} days since we started dating. <br/>Ever since that day, I love you more and more everyday ❤️</p>
+                <p>I hope you are having a wonderful day :&#41;</p>
+                <p className='mt-2'>It&apos;s been <span className='text-red-600'>{datingDays}</span> days since we started dating. <br/>Ever since that day, I love you more and more everyday</p>
             </Parallax>
+
             <Parallax speed={50} translateX={['500px', '-500px']} translateY={['-500px','500px']} className='w-[100px] absolute right-1'>
                 <Image src='/images/shooting-star.png' alt='flower' width={100} height={100} className='' />
             </Parallax>
             <Image src='/images/bf.png' alt='flower' width={400} height={600} className='' />
             <Parallax translateY={[-20, 10]} rotate={[-180,180]}>
-                <Image src='/images/reem/v.png' alt='v' width={100} height={100} />
+                <Image src='/images/reem/shield.png' alt='v' width={100} height={100} />
             </Parallax>
-            <Parallax speed={30}>
+                <p className='mx-2'>내 청춘을 너와 보낼수있어서 행복하고 고마워.<br/> 우리 재밌는거 많이하고 후회없이 사랑하자</p>
+            <Parallax speed={30} className='h-[100px]'>
                 <Image src='/images/reem/flower.jpeg' alt='flower' width={200} height={200} className='absolute top-2 right-[50px]' />
             </Parallax>
-            <Image src='/images/reem/ss.jpg' alt='flower' width={200} height={200} className='' />
-                <Image src='/images/char.png' alt='flower' width={400} height={200} className='mx-auto' />
-
-
-
+            <Parallax speed={20}>
+                <Image src='/images/reem/campingBreakfast.jpeg' alt='flower' width={200} height={200}  className='mx-auto mt-[100px]'/>
+            </Parallax>
+            <Image src='/images/reem/sunsetPose.jpeg' alt='flower' width={200} height={200} className='' />
+            <Image src='/images/us/sunset.jpeg' alt='flower' width={400} height={200} className='mx-auto' />
+            <LaughMachine/>
         </>
     )
 }
---
+++ app/loveyerim/LaughMachine.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+
+// Sample array of photo URLs
+const photos = [
+    '/images/laugh/1.jpeg',
+    '/images/laugh/2.jpeg',
+    '/images/laugh/3.jpeg',
+    '/images/laugh/4.jpeg',
+    '/images/laugh/5.jpeg',
+    '/images/laugh/6.jpeg',
+
+    '/images/laugh/7.jpeg',
+    '/images/laugh/8.jpeg',
+    '/images/laugh/9.jpeg',
+    '/images/laugh/10.jpeg',
+    '/images/laugh/11.jpeg',
+    '/images/laugh/12.jpeg',
+    
+    '/images/laugh/13.jpeg',
+    '/images/laugh/14.jpeg',
+    '/images/laugh/15.jpeg',
+    '/images/laugh/16.jpeg',
+    '/images/laugh/17.jpeg',
+    '/images/laugh/18.jpeg',
+    '/images/laugh/19.jpeg',
+    '/images/laugh/20.jpeg',
+    '/images/laugh/21.jpeg',
+    '/images/laugh/22.jpeg',
+    '/images/laugh/23.jpeg',
+    '/images/laugh/24.jpeg', 
+    '/images/laugh/25.jpeg',
+    '/images/laugh/26.jpeg',
+    '/images/laugh/27.jpeg',
+    '/images/laugh/28.jpeg',
+    '/images/laugh/29.jpeg',
+    '/images/laugh/30.jpeg',
+    '/images/laugh/31.jpeg',
+    '/images/laugh/32.jpeg',
+    '/images/laugh/33.jpeg',
+    '/images/laugh/34.jpeg',
+    '/images/laugh/35.jpeg',
+    '/images/laugh/36.jpeg',
+    '/images/laugh/37.jpeg',
+    '/images/laugh/38.jpeg',
+];
+
+const LaughMachine = () => {
+  // State to hold the index of the current photo
+  const [currentPhotoIndex, setCurrentPhotoIndex] = useState(0);
+
+  // Function to handle button click and set a random photo
+  const handleButtonClick = () => {
+    const randomIndex = Math.floor(Math.random() * photos.length);
+    setCurrentPhotoIndex(randomIndex);
+  };
+
+  return (
+    <div className="flex flex-col items-center h-[200px] mb-2">
+      <img 
+        src={photos[currentPhotoIndex]} 
+        alt="Random" 
+        className="w-3/4 h-auto rounded shadow-lg" 
+      />
+      <button 
+        onClick={handleButtonClick} 
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300"
+      >
+        Laugh Generator 1000 - Click me!
+      </button>
+    </div>
+  );
+};
+
+export default LaughMachine;
